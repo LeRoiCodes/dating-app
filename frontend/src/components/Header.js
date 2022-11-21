c6c1d814b971acd40193422808b2e9f18d82e7cb
LeRoi Vladimir <banyamkimbi@vrfound.org>
2022-11-21 12:28:29 +0100

    Update

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+import PersonIcon from '@material-ui/icons/Person'
+import { IconButton } from '@material-ui/core'
+import ForumIcon from '@material-ui/icons/Forum'
+
+
+
+function Header() {
+  return (
+    <div className='header'></div>
+  )
+}
+
+export default Header
